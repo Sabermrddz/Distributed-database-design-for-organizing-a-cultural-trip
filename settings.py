2fd668851d86dbc312ba377b60b12598e91b23db
saber <sabermrddz@gmail.com>
2026-04-30 13:31:00 +0100

    fix settings for DigitalOcean App Platform deployment

--- settings.py
+++ settings.py
@@ -21,6 +21,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key-change-in-producti
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,testserver').split(',')
+if not DEBUG and '.ondigitalocean.app' not in ALLOWED_HOSTS:
+    ALLOWED_HOSTS.append('.ondigitalocean.app')
 
 # Application definition
 INSTALLED_APPS = [
